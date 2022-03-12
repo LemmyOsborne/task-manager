5ff3e82abcce03fb14d6ce624770df0a2155f5f2
lemmyosborne <bozhenko.rodion@mail.ru>
2022-03-12 17:33:51 +0300

    refactor: improve header styling and add create task button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,8 +5,12 @@ export const Header = () => {
   return (
     <Container>
       <h1>Task Manager</h1>
-      <div>
-        <span style={{ marginRight: "10px" }}>Username</span>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <AddTask>Add Task</AddTask>
+        <span style={{ display: "flex" }}>
+          Logged in as:{" "}
+          <strong style={{ textDecoration: "underline", marginLeft: "5px" }}>Username</strong>
+        </span>
         <Button>Log In</Button>
       </div>
     </Container>
@@ -19,11 +23,13 @@ const Container = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  background-color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
+  -webkit-box-shadow: 0px 5px 8px 0px rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 0px 5px 8px 0px rgba(34, 60, 80, 0.2);
+  box-shadow: 0px 5px 8px 0px rgba(34, 60, 80, 0.2);
 `
 
 const Button = styled.button`
@@ -31,4 +37,19 @@ const Button = styled.button`
   background-color: grey;
   border-radius: 25px;
   color: white;
+`
+
+const AddTask = styled.button`
+  background-color: #019267;
+  padding: 4px 15px;
+  border-radius: 25px;
+  color: white;
+  display: flex;
+  align-items: center;
+
+  :before {
+    content: "+";
+    font-size: 25px;
+    margin-right: 5px;
+  }
 `
